edbc7aabf0b3ca82183542930e6744c3d8966b7e
murilloressineti <murillobr22@gmail.com>
2023-10-23 15:43:22 -0300

    Funções

---
+++ js/functions/functions.js
@@ -0,0 +1,13 @@
+//Criar um aplicativo de frases motivacionais
+
+//Declaração da função / Function statement
+function createPhrases(){
+    console.log('Estuadar é muito bom!')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+//Executar a função 
+createPhrases()
+
+console.log('Fim do programa')
