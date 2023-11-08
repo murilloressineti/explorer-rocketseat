004b8fafc12fdb5087ef5fa7210fc95b4bc850fe
murilloressineti <murillobr22@gmail.com>
2023-11-08 11:47:47 -0300

    Trabalhando com objects

--- js/algoritimos-e-logica-de-programacao/script.js
+++ js/algoritimos-e-logica-de-programacao/script.js
@@ -8,6 +8,8 @@
     altura
 
   Escreva uma lista contendo o nome dos pacientes suas idades, alturas e pesos.
+
+  Exemplo: Luiz tem 20 anos, pesa 100kg e tem 190 de altura.
 */
 
 const patients = [
@@ -32,3 +34,30 @@ const patients = [
         heigth: 180,
     },
 ]
+
+let patientsNames = []
+let patientsAges = []
+let patientsWeigths = []
+let patientsHeights = []
+
+for(let patient of patients) {
+  patientsNames.push(patient.name)
+}
+
+for(let patient of patients){
+  patientsAges.push(patient.age)
+}
+
+for(let patient of patients){
+  patientsWeigths.push(patient.weigth)
+}
+
+for(let patient of patients){
+  patientsHeights.push(patient.heigth)
+}
+
+alert(`${patientsNames[0]} tem ${patientsAges[0]} anos, pesa ${patientsWeigths[0]}kg e tem ${patientsHeights[0]} de altura.`)
+
+alert(`${patientsNames[1]} tem ${patientsAges[1]} anos, pesa ${patientsWeigths[1]}kg e tem ${patientsHeights[1]} de altura.`)
+
+alert(`${patientsNames[2]} tem ${patientsAges[2]} anos, pesa ${patientsWeigths[2]}kg e tem ${patientsHeights[2]} de altura.`)
