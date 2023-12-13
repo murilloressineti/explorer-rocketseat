689fad6dd129c099c608deb6999ecfba3cc2d54d
murilloressineti <murillobr22@gmail.com>
2023-12-13 15:26:23 -0300

    update-js

---
+++ js/es6-modules/main.js
@@ -0,0 +1,4 @@
+// main.js
+import qualquerNome from './utils.js' // nessa linha ele importa e já executa o arquivo
+
+console.log(qualquerNome)
---
+++ js/es6-modules/utils.js
@@ -0,0 +1,4 @@
+// utils.js
+const message = "mensagem" // essa variável fica isolada nesse arquivo, e não consigo usar ela em outro lugar
+
+export default message
