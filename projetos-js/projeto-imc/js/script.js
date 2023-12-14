762e458683bbea4f3e49163ec6b44f7342b40a81
murilloressineti <murillobr22@gmail.com>
2023-12-14 14:33:27 -0300

    update

--- projetos-js/projeto-imc/js/script.js
+++ projetos-js/projeto-imc/js/script.js
@@ -1,38 +1,33 @@
-//variáveis
+import {modal} from './modal.js'
+import {alertError} from './alert-error.js'
+import {calculateIMC, notANumber} from './utils.js'
+
 const form = document.querySelector('form')
 const inputWeight =  document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modal = {
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-    open() {
-        modal.wrapper.classList.add('open')
-    },
-    close(){
-        modal.wrapper.classList.remove('open')
-    }
-}
-
 form.onsubmit = function(event) {
-    event.preventDefault() // Não faça o padrão. Explicando: o botão está dentro de um formulário, o padrão seria enviar o formulário (submit).
+    event.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const result = imc(weight, height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+    if(weightOrHeightIsNotANumber) {
+        alertError.open()
+        return;
+    }
+
+    alertError.close()
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
     const message = `Seu IMC é de ${result}`
 
     modal.message.innerText = message
     modal.open()
-}
-
-modal.buttonClose.onclick = () => {
-    modal.close()
-}
-
-function imc(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
-}
+    }
