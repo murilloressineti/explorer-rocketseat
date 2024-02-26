0c9dc4dfc7a205c534f27f95cc0c459e4f23e648
murilloressineti <murillobr22@gmail.com>
2024-02-26 16:26:18 -0300

    update

---
+++ desafios/stage-08/api/src/controllers/MoviesController.js
@@ -0,0 +1,35 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
+class MoviesController {
+  async create(request, response) {
+    const { title, description, rating, tags } = request.body
+    const { user_id } = request.params
+
+    if (rating < 1 || rating > 5) {
+        throw new AppError(`O valor do campo 'rating' deve estar entre 1 e 5.`)
+    }
+
+    const [MovieNote_id] = await knex("MovieNotes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    /*const tagsInsert = tags.map(name => {
+      return {
+        MovieNotes_id,
+        name,
+        user_id
+      }
+    })
+
+    await knex("MovieTags").insert(tagsInsert)*/
+
+    response.json()
+  }
+
+}
+
+module.exports = MoviesController
--- desafios/stage-08/api/src/routes/index.js
+++ desafios/stage-08/api/src/routes/index.js
@@ -1,8 +1,10 @@
-const { Router } = require("express")
+const { Router } = require('express')
 
-const usersRouter = require('./users.routes')
+const usersRoutes = require('./users.routes')
+const moviesRoutes = require('./movies.routes')
 
 const routes = Router()
-routes.use('/users', usersRouter)
+routes.use('/users', usersRoutes)
+routes.use('/movies', moviesRoutes)
 
 module.exports = routes
