c990966b29e69f0de341c5b013f429955e23b214
Murillo Silva <murillobr22@gmail.com>
2024-03-01 16:47:30 -0300

    react

---
+++ react/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import {Container} from './styles.js'
+
+export function Details() {
+
+  return (
+    <Container>
+      <h1>Hello Wolrd!</h1>
+    </Container>
+  )
+}
---
+++ react/rocketnotes/src/pages/Details/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+
+    background-color: rgb(255, 191, 191);
+    color: blue;
+
+`
