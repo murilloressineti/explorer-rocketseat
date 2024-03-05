6ad66c4a3f9d01133b822abb5dc5b56c29d72c73
Murillo Silva <murillobr22@gmail.com>
2024-03-05 16:28:43 -0300

    update details

--- react/rocketnotes/src/pages/Details/index.jsx
+++ react/rocketnotes/src/pages/Details/index.jsx
@@ -1,14 +1,47 @@
-import { Container } from './styles.js'
+import { Container, Links, Content } from './styles.js'
 
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
-
   return (
     <Container>
       <Header/>
-      <Button title="Voltar" />
+
+      <main>
+        <Content>
+
+          <ButtonText title="Excluir nota"/>
+
+          <h1>Introdução ao React</h1>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur dolorum nisi necessitatibus ex a ipsam hic, repellat magnam dolor soluta repudiandae ut cumque tempore! Repudiandae aut iste dolores culpa!</p>
+
+          <Section title="Links úteis" >
+            <Links>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores" >
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+
+
+          <Button title="Voltar" />
+      </Content>
+      </main>
+
     </Container>
   )
 }
