8da4f6250059e825d18bbc5b049996f51539cd63
Murillo Silva <murillobr22@gmail.com>
2024-03-06 16:45:53 -0300

    update react

---
+++ react/rocketnotes/src/pages/Home/index.jsx
@@ -0,0 +1,43 @@
+import { FiPlus } from 'react-icons/fi'
+import { Container, Brand, Menu, Search, Content, NewNote  } from './styles'
+
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { ButtonText } from '../../components/ButtonText'
+
+export function Home() {
+    return (
+        <Container>
+            <Brand>
+                <h1>Rocketnotes</h1>
+            </Brand>
+
+            <Header></Header>
+
+            <Menu>
+                <li>
+                    <ButtonText title="Todos" $isactive/>
+                </li>
+                <li>
+                    <ButtonText title="React"/>
+                </li>
+                <li>
+                    <ButtonText title="Nodejs"/>
+                </li>
+            </Menu>
+
+            <Search>
+                <Input placeholder="Pesquisar pelo título"/>
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewNote>
+                <FiPlus />
+                Criar nota
+            </NewNote>
+        </Container>
+    )
+}
