b78c4496482429fe0cde1a3f983e9cee3aed0ae2
Murillo Silva <murillobr22@gmail.com>
2024-03-12 16:07:28 -0300

    update rocketmovies

---
+++ desafios/stage-09/rocketmovies/src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles.js"
+
+import { Button } from "../../components/Button"
+
+export function Details() {
+  return (
+    <Container>
+      <Button title='oi'/>
+      <h1>Oi</h1>
+    </Container>
+  )
+}
---
+++ desafios/stage-09/rocketmovies/src/styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+    COLORS: {
+      BACKGROUND_900: "#0D0C0F",
+      BACKGROUND_800: "#1C1B1E",
+      BACKGROUND_700: "#262529",
+
+      TAGS: "#282124",
+      TAGS_COLOR: "#E5E5E5",
+      
+      WHITE: "#FFFFFF",
+      PINK: "#FF859B",
+  
+      GRAY_100: "#F4EDE8",
+      GRAY_200: "#CAC4CF",
+      GRAY_300: "#948F99",
+      GRAY_400: "#999591",
+    }
+  }
